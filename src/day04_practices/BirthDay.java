af1f20e775e30cc33638b88d64edc0a13e63fdd3
bakhromizzatullaev <bakhromizzatulife@gmail.com>
2024-04-22 16:01:32 -0700

    day04_Practices

--- src/day04_practices/BirthDay.java
+++ src/day04_practices/BirthDay.java
@@ -4,11 +4,11 @@ public class BirthDay {
 
     public static void main(String[] args) {
         String name = "John",
-                birhtMonth = "April";
-        int birhtDay = 25,
+                birthMonth = "April";
+        int birthDay = 25,
                 birthYear = 1995;
 
-        System.out.println(name + " was born on " + birhtMonth + "/" + birhtDay + "/" + birthYear + ".");
+        System.out.println(name + " was born on " + birthMonth + "/" + birthDay + "/" + birthYear + ".");
 
 
 
