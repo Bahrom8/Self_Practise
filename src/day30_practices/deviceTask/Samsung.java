42b6baf32152144781227199f9e8d47ffc071fbc
bakhromizzatullaev <bakhromizzatulife@gmail.com>
2024-04-25 13:52:05 -0700

    day30_Practices

---
+++ src/day30_practices/deviceTask/Samsung.java
@@ -0,0 +1,9 @@
+package day30_practices.deviceTask;
+
+public class Samsung extends Phone{
+
+    public Samsung(String brand, String model, String color, int size, double price, boolean hasBattery, boolean hasPowerButton) {
+        super(brand, model, color, size, price, hasBattery, hasPowerButton);
+    }
+
+}
